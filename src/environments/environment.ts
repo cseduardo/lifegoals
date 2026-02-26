dc4741086783087978cfb6d1b56f9c0ab6beb214
Eduardo Campos S <eduardo_c.s@outlook.com>
2026-02-26 06:23:36 -0600

    Fix: Subiendo archivos environment para Docker

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase:{
+        apiKey: "AIzaSyAEVMij3bc9-slcROZWWeKFVf0vLsYRVCk",
+  authDomain: "my-cv-498b0.firebaseapp.com",
+  projectId: "my-cv-498b0",
+  storageBucket: "my-cv-498b0.firebasestorage.app",
+  messagingSenderId: "785888210310",
+  appId: "1:785888210310:web:f67185438047a7089b5007",
+  measurementId: "G-HC60Z12FP5"
+    }
+};
